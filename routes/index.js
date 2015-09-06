f514cb65def2e7799f1a7e9f4fdaeca55cd26c4e
akshat <always.akshat@gmail.com>
2015-09-07 00:38:30 +0530

    redirect random pages to login

--- routes/index.js
+++ routes/index.js
@@ -67,4 +67,24 @@ module.exports = function(app) {
 
         });
 
+    app.route('/*')
+        .get(function(req, res) {
+            var options = {
+                root: app.get('appPath'),
+                dotfiles: 'deny',
+                headers: {
+                    'x-timestamp': Date.now(),
+                    'x-sent': true
+                }
+            };
+            var fileName = 'login.html';
+            res.sendFile(fileName, options, function (err) {
+                if (err) {
+                    res.status(err.status).end();
+                }
+                else {
+                    console.log('Sent:', fileName);
+                }
+            });
+        });
 };
